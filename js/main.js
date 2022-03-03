b9b6c5ef82fa00c4f9bdf6951153f21aac611e2d
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-03-03 21:49:29 +0900

    isotope plugin, responsive css control

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Page load event
+window.addEventListener("load", ()=> {
+    const grid = new Isotope("section", {
+        itemSelector: "article",
+        columnWidth: "article",
+        transitionDuration: "0.5s"
+    });
+});
