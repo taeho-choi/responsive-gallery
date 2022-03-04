0a09548796db59ccf5ad72eff285751899eac198
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-03-04 17:04:18 +0900

    Classification function

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ window.addEventListener("load", ()=> {
         columnWidth: "article",
         transitionDuration: "0.5s"
     });
+
+    const btns = document.querySelectorAll("main ul li");
+
+    for(let el of btns) {
+        el.addEventListener("click", e=> {
+            e.preventDefault();
+
+            const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+            grid.arrange({
+                filter: sort
+            });
+
+            for(let el of btns) {
+                el.classList.remove("on");
+            }
+
+            e.currentTarget.classList.add("on");
+        })
+    }
 });
